e98faef8fb9c98e6c932068ec7aab8808a383de3
Assassins-white <assassins-white@foxmail.com>
2022-07-18 11:20:50 +0800

    更新结果去重

--- Name2Pinyin.py
+++ Name2Pinyin.py
@@ -5,13 +5,18 @@ from xpinyin import Pinyin
 p = Pinyin()
 filename = input('输入需要转换的文件名:')
 savefile = open('save.txt','w')
+parame = set()
 with open(filename) as f:
 	for i in f.readlines():
 	    result = p.get_pinyin(i)
 	    s = result.split('-')
 	    result3 = s[0] + ''.join(s[1:])
 	    print(result3.strip())
-	    savefile.write(result3)
-	savefile.close()
-	print("#"*50)
-	print('文件转换成功，输出文件 --> save.txt')
+	    parame.add(result3.strip())
+
+for i in parame:
+	savefile.write(i + "\n")
+
+print("#"*50)
+print('文件转换成功，输出文件(结果已去重) --> save.txt')
+savefile.close()
